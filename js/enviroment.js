507fce7063cc1128e480794c4ff2a3bcebd0ab7c
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2022-11-13 15:59:22 -0600

    estructura de los archivos, firebase y article crud

---
+++ js/enviroment.js
@@ -0,0 +1,5 @@
+const environment = {
+    urlDb: 'https://kodemiadevto-default-rtdb.firebaseio.com/'
+};
+
+export default environment;
---
+++ js/main.js
@@ -0,0 +1,28 @@
+import environment from "./enviroment.js";
+const urlColeccion = environment.urlDb + '/articles.json';
+
+
+
+
+const articles = {
+    cover_image: 'https://www.school-for-champions.com/science/images/static_generating_wimshurst.jpg',
+    title: 'Prueba Kodemia',
+    tags: 'kodemia, alfred, vic, grace, alejandro',
+    parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptates illo asperiores! Reiciendis totam ut neque commodi fugit labore itaque, rem doloremque, ipsa expedita dolor voluptas quod nam ad unde.',    
+};
+
+
+fetch(urlColeccion, {
+    method: 'POST',
+    body: JSON.stringify(articles),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+
+console.log(environment);
